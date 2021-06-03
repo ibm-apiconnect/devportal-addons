3496ed359a78e6f91033b94f2eb4a247a92acae9
Chris Dudley <chris.dudley@uk.ibm.com>
2021-06-03 16:09:17 +0100

    Example module to block users by domains

---
+++ apic_v10/modules/domain_block/README.md
@@ -0,0 +1,9 @@
+# Domain block validation example
+
+This module shows how to do custom validation of user's at user registration.
+In this scenario the module is checking their email address domain is not on the blocked list provided by the portal administrator.
+
+### Note: This module requires APIC v2018 / v10, it will not work in APIC v5.
+
+This module is provided as-is as a sample.
+It is licensed under the GPL v2.
---
+++ apic_v10/modules/domain_block/src/Form/ConfigForm.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\domain_block\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Domain block settings form.
+ */
+class ConfigForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID(): string {
+    return 'domain_block_config_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['domain_block.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $config = $this->config('domain_block.settings');
+
+    $currentValue = $config->get('banned_domains');
+
+    $form['intro'] = [
+      '#markup' => t('Domain Block Settings'),
+      '#weight' => -20,
+    ];
+
+    $form['domains'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Domains'),
+      '#collapsible' => TRUE,
+      '#collapsed' => FALSE,
+    ];
+    $form['domains']['banned_domains'] = [
+      '#type' => 'textarea',
+      '#title' => t('Banned domains'),
+      '#description' => t('List the banned domains separated by new lines.'),
+      '#default_value' => $currentValue,
+      '#required' => FALSE,
+      '#wysiwyg' => FALSE,
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    // Set the submitted configuration settings
+    $banned_domains = $form_state->getValue('banned_domains');
+
+    $this->config('domain_block.settings')
+      ->set('banned_domains', $banned_domains)
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
