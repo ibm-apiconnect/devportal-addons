3ed3d6719d376d976f4f3bdbf190fe47ef34db52
Anne Redwood <anne.redwood@uk.ibm.com>
2023-02-23 18:10:50 +0000

    Update for Drupal 10 compatibility

--- apic_v10/modules/change_account_display/src/Routing/UsernameRouteSubscriber.php
+++ apic_v10/modules/change_account_display/src/Routing/UsernameRouteSubscriber.php
@@ -1,5 +1,14 @@
 <?php
-
+/********************************************************* {COPYRIGHT-TOP} ***
+ * Licensed Materials - Property of IBM
+ * 5725-L30, 5725-Z22
+ *
+ * (C) Copyright IBM Corporation 2016, 2022
+ *
+ * All Rights Reserved.
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ ********************************************************** {COPYRIGHT-END} **/
 namespace Drupal\change_account_display\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
